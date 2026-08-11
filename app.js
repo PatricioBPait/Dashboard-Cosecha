7b6993ce7f09df98e0ebd3405bc4a29aaa146c3c
PatricioBPait <Patriciobuffoh@gmail.com>
2026-08-11 08:01:40 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
 document.addEventListener("DOMContentLoaded", function () {
 
-    fetch("datos.json")
+    const URL_PARTE_DIARIO =
+  "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRpElAT0stTkIdi4rF9mhzOlbjrz7pvlP_0R623W6MmbTyyME0yEOic-rA3b99lK9CNnZIz7TuZOW7/pub?gid=811133446&single=true&output=csv";
+
+fetch(URL_PARTE_DIARIO)
+  .then(response => {
+    if (!response.ok) {
+      throw new Error("No se pudo acceder a Google Sheets");
+    }
+    return response.text();
+  })
+  .then(csv => {
+    console.log("Datos recibidos desde Google Sheets:");
+    console.log(csv);
+
+    // Acá vamos a convertir el CSV
+    // y alimentar el dashboard.
+  })
+  .catch(error => {
+    console.error("Error cargando Google Sheets:", error);
+  });
         .then(function (respuesta) {
 
             if (!respuesta.ok) {
