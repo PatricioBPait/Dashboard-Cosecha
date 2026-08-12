d90adbe1c1119a0aa66687e2a3da3ef1304de436
PatricioBPait <Patriciobuffoh@gmail.com>
2026-08-12 07:39:04 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,46 @@
-```javascript
 document.addEventListener("DOMContentLoaded", function () {
 
-    console.log("APP.JS funcionando correctamente");
+    console.log("APP.JS FUNCIONANDO");
 
     const URL_PARTE_DIARIO =
         "https://docs.google.com/spreadsheets/d/e/2PACX-1vSRpElAT0stTkIdi4rF9mhzOlbjrz7pvlP_0R623W6MmbTyyME0yEOic-rA3b99lK9CNnZIz7TuZOW7/pub?gid=811133446&single=true&output=csv";
 
     fetch(URL_PARTE_DIARIO)
         .then(function (respuesta) {
-            console.log("Google Sheets respondió:", respuesta.status);
+
+            console.log(
+                "Respuesta Google Sheets:",
+                respuesta.status
+            );
+
+            if (!respuesta.ok) {
+                throw new Error(
+                    "Google Sheets respondió con error"
+                );
+            }
+
             return respuesta.text();
         })
+
         .then(function (csv) {
-            console.log("CSV recibido correctamente");
-            console.log(csv.substring(0, 500));
+
+            console.log(
+                "CSV RECIBIDO CORRECTAMENTE"
+            );
+
+            console.log(
+                csv.substring(0, 500)
+            );
+
         })
+
         .catch(function (error) {
-            console.error("ERROR:", error);
+
+            console.error(
+                "ERROR:",
+                error
+            );
+
         });
 
 });
